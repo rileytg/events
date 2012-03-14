79c0601cfbba933bdae3f80ca480b98bf68df3ae
Riley Guerin <rileytg@mpro.local>
2012-03-14 01:40:21 -0700

    monthly loading events

--- src/main/java/tap/hw/domain/Event.java
+++ src/main/java/tap/hw/domain/Event.java
@@ -5,16 +5,23 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
+
+import org.apache.commons.lang3.time.DateUtils;
+import org.joda.time.DateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.roo.addon.equals.RooEquals;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 
 @RooJavaBean
 @RooToString
 @RooEquals
 @RooJpaActiveRecord(finders = { "findEventsByEventDateBetween" })
+@RooJson
+@RooWebJson(jsonObject = tap.hw.domain.Event.class)
 public class Event {
 
     @ManyToOne
@@ -35,4 +42,16 @@ public class Event {
     private String hwScore;
 
     private String opponentScore;
+
+    private transient String title;
+
+    private transient Long start;
+
+    public String getTitle() {
+        return team.toString();
+    }
+
+    public Long getStart() {
+        return new DateTime(eventDate).toInstant().getMillis()/1000;
+    }
 }
--- src/main/java/tap/hw/web/CalendarController.java
+++ src/main/java/tap/hw/web/CalendarController.java
@@ -1,20 +1,20 @@
 package tap.hw.web;
 
-import javax.persistence.TypedQuery;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang3.time.DateUtils;
 import org.joda.time.DateTime;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import tap.hw.domain.Event;
 
+import javax.persistence.TypedQuery;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RequestMapping("/calendar")
@@ -22,14 +22,15 @@ import java.util.List;
 public class CalendarController {
 
     @RequestMapping(value = "eventFeed")
-    public String events( @RequestParam String start, @RequestParam String end, ModelMap modelMap,Model uiModel, HttpServletRequest request, HttpServletResponse response) {
+    public ResponseEntity<String> events( @RequestParam String start, @RequestParam String end, ModelMap modelMap,Model uiModel, HttpServletRequest request, HttpServletResponse response) {
         DateTime startDateTime = new DateTime(Long.parseLong(start));
         DateTime endDateTime = new DateTime(Long.parseLong(end));
         TypedQuery<Event> query = Event.findEventsByEventDateBetween(startDateTime.toDate(), endDateTime.toDate());
         List<Event> eventList = query.getResultList();
-        uiModel.addAttribute("events", eventList);
-
-        return "events/list";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        List<Event> result = Event.findAllEvents();
+        return new ResponseEntity<String>(Event.toJsonArray(result), headers, HttpStatus.OK);
     }
 
 
